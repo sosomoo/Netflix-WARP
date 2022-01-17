395de9d420d1af53269139174aa4c26030a0a79c
kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-01-18 07:15:33 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+[[ -e /root/check.sh ]] && screen -S aw -X quit ; screen -dmS aw bash -c '/bin/bash /root/check.sh'
 green(){ echo -e "\033[32m\033[01m$1\033[0m";}
 yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
 wgcfv4=$(curl -s4m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
