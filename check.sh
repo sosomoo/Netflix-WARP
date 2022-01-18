fbeb069e29e9cb99e5863bd9451410eba09ce41d
kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-01-18 08:46:02 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
-[[ -e /root/check.sh ]] && screen -S aw -X quit ; screen -dmS aw bash -c '/bin/bash /root/check.sh'
+[[ -e /root/check.sh ]] && screen -S aw -X quit
+screen -dmS bc;
+screen -S bc -p 0 -X stuff "/bin/bash /root/check.sh";  
+screen -S bc -p 0 -X stuff $'\n'
 green(){ echo -e "\033[32m\033[01m$1\033[0m";}
 yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
 wgcfv4=$(curl -s4m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
