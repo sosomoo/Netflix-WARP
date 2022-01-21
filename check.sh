10755b8ed4b0aa63e7107e12217dd52cdf36d82d
kkkyg <90416692+kkkyg@users.noreply.github.com>
2022-01-21 19:39:30 +0800

    Update check.sh

--- check.sh
+++ check.sh
@@ -2,6 +2,7 @@
 sleep 5
 green(){ echo -e "\033[32m\033[01m$1\033[0m";}
 yellow(){ echo -e "\033[33m\033[01m$1\033[0m";}
+info(){
 wgcfv4=$(curl -s4m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2) 
 wgcfv6=$(curl -s6m3 https://www.cloudflare.com/cdn-cgi/trace -k | grep warp | cut -d= -f2)
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
@@ -9,39 +10,47 @@ result4=$(curl -4 --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --o
 result6=$(curl -6 --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
 mport=`warp-cli --accept-tos settings 2>/dev/null | grep 'Proxy listening on' | awk -F "127.0.0.1:" '{print $2}'`
 result=$(curl -sx socks5h://localhost:$mport -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1) 
+}
 s5c(){
 warp-cli --accept-tos register >/dev/null 2>&1 && sleep 2
 [[ -e /etc/wireguard/ID ]] && warp-cli --accept-tos set-license $(cat /etc/wireguard/ID) >/dev/null 2>&1
 }
+info
 WGCFV4(){
 while true; do
+info
 [[ "$result4" == "200" ]] && green "目前wgcf-ipv4的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv4的IP不支持奈飞，刷新wgcf-ipv4的IP中……" && sleep 30s)
 done
 }
 WGCFV6(){
 while true; do
+info
 [[ "$result6" == "200" ]] && green "目前wgcf-ipv6的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv6的IP不支持奈飞，刷新wgcf-ipv6的IP中……" && sleep 30s)
 done
 }
 SOCKS5warp(){
 while true; do
+info
 [[ "$result" == "200" ]] && green "目前socks5的IP支持奈飞，停止刷新" && sleep 45s || (s5c && yellow "目前socks5的IP不支持奈飞，刷新socks5的IP中……" && sleep 30s)
 done
 }
 SOCKS5wgcf4(){
 while true; do
+info
 [[ "$result" == "200" ]] && green "目前socks5的IP支持奈飞，停止刷新" && sleep 45s || (s5c && yellow "目前socks5的IP不支持奈飞，刷新socks5的IP中……" && sleep 30s)
 [[ "$result4" == "200" ]] && green "目前wgcf-ipv4的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv4的IP不支持奈飞，刷新wgcf-ipv4的IP中……" && sleep 30s)
 done
 }
 SOCKS5wgcf6(){
 while true; do
+info
 [[ "$result" == "200" ]] && green "目前socks5的IP支持奈飞，停止刷新" && sleep 45s || (s5c && yellow "目前socks5的IP不支持奈飞，刷新socks5的IP中……" && sleep 30s)
 [[ "$result6" == "200" ]] && green "目前wgcf-ipv6的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv6的IP不支持奈飞，刷新wgcf-ipv6的IP中……" && sleep 30s)
 done
 }
 WGCFV4V6(){
 while true; do
+info
 [[ "$result4" == "200" ]] && green "目前wgcf-ipv4的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv4的IP不支持奈飞，刷新wgcf-ipv4的IP中……" && sleep 30s)
 [[ "$result6" == "200" ]] && green "目前wgcf-ipv6的IP支持奈飞，停止刷新" && sleep 45s || (systemctl restart wg-quick@wgcf && yellow "目前wgcf-ipv6的IP不支持奈飞，刷新wgcf-ipv6的IP中……" && sleep 30s)
 done
